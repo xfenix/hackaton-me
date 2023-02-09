bb1b8d0cf0affd8497d5d0cec79f334bf1d9df4e
Evgeny Perchun <evgeny.perchun@raiffeisen.ru>
2023-02-09 22:11:18 +0300

    Fix get-event-info url

--- backend/cashapp/urls.py
+++ backend/cashapp/urls.py
@@ -6,5 +6,5 @@ from cashapp import views
 urlpatterns = [
     path('make-order/', views.MakeOrderView.as_view(), name='make_order'),
     path('make-qr/<str:event_id>/', views.MakeQr.as_view(), name='make_qr'),
-    path('get-event-info/<int:event_qr_code_id>', views.FetchEventView.as_view(), name='fetch_event_info'),
+    path('get-event-info/<int:event_qr_code_id>/', views.FetchEventView.as_view(), name='fetch_event_info'),
 ]
