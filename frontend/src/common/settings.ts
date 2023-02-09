be0d5b5b6b983c6ec39f1b198b6d9e0d33df84d4
Denis Anikin <ad@xfenix.ru>
2023-02-10 00:07:34 +0300

    Kekestan

--- frontend/src/common/settings.ts
+++ frontend/src/common/settings.ts
@@ -1,6 +1,8 @@
 export const STYLE_BORDER_RADIUS = 12;
 export const COLOR_BLACK = "#2B2D33";
 export const COLOR_VERY_LIGHT_GRAY = "#e1e1e3";
+export const COLOR_LIGHT_GRAY = "#A2A4A6";
+export const COLOR_BRAND = "#FEE600";
 export const DEFAULT_FONT_NAME = "ALS Hauss";
 export const DEFAULT_BACKGROUND = "party2";
 export const PNG_LOGOS = ["ontico", "pycon"];
--- frontend/src/screens/CheckoutScreen/CheckoutScreen.tsx
+++ frontend/src/screens/CheckoutScreen/CheckoutScreen.tsx
@@ -2,12 +2,13 @@ import { MainLayout } from "../../components/MainLayout";
 import styled from "styled-components";
 import * as settings from "../../common/settings";
 
+const TYPICAL_PADDING = 20;
 export const FormWrapper = styled.form`
   margin-top: 32px;
 
   & label > span {
-    padding-bottom: 12px;
-    margin-left: 20px;
+    margin-bottom: 8px;
+    margin-left: ${TYPICAL_PADDING}px;
     display: block;
   }
 
@@ -17,13 +18,51 @@ export const FormWrapper = styled.form`
     background: #ffffff;
     border: 1px solid ${settings.COLOR_VERY_LIGHT_GRAY};
     border-radius: 8px;
-    padding: 14px 20px;
+    padding: 14px ${TYPICAL_PADDING}px;
     box-sizing: border-box;
     outline: none;
   }
 `;
 export const FormRow = styled.div``;
-export const FormSeprator = styled.div``;
+export const FormSeparator = styled.div`
+  color: ${settings.COLOR_LIGHT_GRAY};
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  margin: 24px 0 16px 0;
+  padding: 0 ${TYPICAL_PADDING}px;
+  font-size: 12px;
+
+  &::before,
+  &::after {
+    display: block;
+    content: "";
+    background: ${settings.COLOR_LIGHT_GRAY};
+    height: 1px;
+    width: 100%;
+  }
+`;
+export const SubmitButton = styled.button`
+  margin-top: 32px;
+  background: ${settings.COLOR_BRAND};
+  border-radius: 8px;
+  padding: 14px ${TYPICAL_PADDING}px;
+  display: flex;
+  align-items: center;
+  border: 1px solid ${settings.COLOR_VERY_LIGHT_GRAY};
+  transition: background-color 0.4s ease;
+  width: 100%;
+  box-sizing: border-box;
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+
+  &:hover {
+    background: #fed500;
+  }
+`;
 
 export const CheckoutScreen = () => {
   return (
@@ -40,13 +79,31 @@ export const CheckoutScreen = () => {
             <input type="email" name="email" placeholder="Ваш email" />
           </label>
         </FormRow>
-        <FormSeprator>Или</FormSeprator>
+        <FormSeparator>
+          <div>Или</div>
+        </FormSeparator>
         <FormRow>
           <label>
             <span>Телефон:</span>
             <input type="tel" name="phone" placeholder="Ваш телефон" />
           </label>
         </FormRow>
+        <SubmitButton type="submit">
+          <svg
+            width="24"
+            height="20"
+            viewBox="0 0 24 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M16.8 10.8H19.2V8.4H16.8V10.8ZM15.6 12C14.28 12 13.2 10.92 13.2 9.6C13.2 8.28 14.28 7.2 15.6 7.2H21.6V12H15.6ZM2.4 16.8V2.4H19.2V4.8H15.6C12.948 4.8 10.8 6.948 10.8 9.6C10.8 12.252 12.948 14.4 15.6 14.4H19.2V16.8H2.4ZM1.2 19.2H20.4C21.06 19.2 21.6 18.66 21.6 18V16.8C21.6 15.636 21.012 14.868 19.992 14.556V14.4H22.8C23.46 14.4 24 13.86 24 13.2V6C24 5.34 23.46 4.8 22.8 4.8H19.992V4.644C21.012 4.332 21.6 3.564 21.6 2.4V1.2C21.6 0.54 21.06 0 20.4 0H1.2C0.54 0 0 0.54 0 1.2V18C0 18.66 0.54 19.2 1.2 19.2Z"
+              fill="#2B2D33"
+            />
+          </svg>
+          <span>Оплатить</span>
+          <strong>1 585 ₽</strong>
+        </SubmitButton>
       </FormWrapper>
     </MainLayout>
   );
