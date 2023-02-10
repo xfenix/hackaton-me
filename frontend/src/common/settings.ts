82dc56c59bbfcf04428d0b6a176ee8b160323f97
Alexander Mikhaylov <aleksandr.i.mikhaylov@raiffeisen.ru>
2023-02-10 05:12:11 +0300

    Better borders

--- frontend/src/common/settings.ts
+++ frontend/src/common/settings.ts
@@ -3,6 +3,7 @@ export const COLOR_BLACK = "#2B2D33";
 export const COLOR_VERY_LIGHT_GRAY = "#e1e1e3";
 export const COLOR_LIGHT_GRAY = "#A2A4A6";
 export const COLOR_BRAND = "#FEE600";
+export const COLOR_RED = "#EE505A";
 export const DEFAULT_FONT_NAME = "ALS Hauss";
 export const DEFAULT_BACKGROUND = "party2";
 export const PNG_LOGOS = ["ontico", "pycon"];
